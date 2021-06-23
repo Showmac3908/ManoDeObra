20c33ba2801dbd6bda46d1c53c89d16ee2f29bc3
Showmac3908 <marketing@showmac.com.mx>
2021-06-23 14:37:06 -0500

    Mano de obra

---
+++ js/contact.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.contact-text', {delay: 600});
+ScrollReveal().reveal('.insta', {delay: 1400});
+ScrollReveal().reveal('.fb', {delay: 1200});
+ScrollReveal().reveal('.wa', {delay: 1600});
---
+++ js/main.js
@@ -0,0 +1,19 @@
+const btnToggle = document.querySelector('.toggle-btn');
+const sidebar = document.getElementById('sidebar');
+const body = document.querySelector('body');
+
+btnToggle.addEventListener('click', function (e) {
+    e.stopPropagation();
+    sidebar.classList.toggle('active');
+});
+
+body.addEventListener('click', function () {
+    if(sidebar.classList.contains('active')) {
+        sidebar.classList.toggle('active');
+    }
+});
+
+ScrollReveal().reveal('.element1');
+ScrollReveal().reveal('.element2', {delay: 200});
+ScrollReveal().reveal('.element3', {delay: 600});
+ScrollReveal().reveal('.element4', {delay: 800});
